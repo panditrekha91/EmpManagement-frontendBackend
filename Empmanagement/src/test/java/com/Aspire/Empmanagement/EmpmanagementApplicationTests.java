c4f63c6a96f81e7e94b75608302cd3d384322d0c
Rekha Pandit <panditrekha91@gmail.com>
2023-02-15 11:40:49 +0530

    first commit where just add project with working all fun

---
+++ Empmanagement/src/test/java/com/Aspire/Empmanagement/EmpmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aspire.Empmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
